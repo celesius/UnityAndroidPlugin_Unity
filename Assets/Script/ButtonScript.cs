93668bebddece942e9d95ca09337a71a8c35922c
celesius <188166614@qq.com>
2017-02-19 13:26:54 +0800

    上传使用Android插件的Unity代码

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void callButton(){
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		AndroidJavaClass jc = new AndroidJavaClass("com.clover.unityplugforblog.NextActivity");
+		// Calling a Call method to which the current activity is passed
+		jc.CallStatic("Call", currentActivity);
+		//currentActivity.Call ("jump_view");
+
+		AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+	}
+}
